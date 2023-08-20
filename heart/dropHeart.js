25e40fe3a75cd56d9ebf4d3e0e4ec67538b280c9
zyzuung <zyzuung199@gmail.com>
2023-08-20 21:23:35 +0700

    commit code

--- heart/dropHeart.js
+++ heart/dropHeart.js
@@ -1,3 +1,96 @@
+var slideIndex = 0;
+var currentSlideIndex = 0;
+var slideArray = [];
+
+function Slide(title, subtitle, background) {
+  this.title = title;
+  this.subtitle = subtitle;
+  this.background = background;
+  this.id = "slide" + slideIndex;
+  slideIndex++;
+  slideArray.push(this);
+}
+var walkingDead = new Slide(
+  "Tiến Chương * Thục Đoan",
+  "24/10/2023",
+  "../image/13662403df40419741a2858e38135a5c.jpg"
+);
+
+var bigBang = new Slide(
+  "Tiến Chương * Thục Đoan",
+  "24/10/2023",
+  "../image/568603cbd1860c67bf8f6776cbe7f885.jpg"
+);
+
+var LastMan = new Slide(
+  "Tiến Chương * Thục Đoan",
+  "24/10/2023",
+  "../image/Hinh-anh-binh-yen-nhat.jpg"
+);
+
+function buildSlider() {
+  var myHTML;
+  for (var i = 0; i < slideArray.length; i++) {
+    myHTML +=
+      "<div id='" +
+      slideArray[i].id +
+      "' class='singleSlide' style='background-image:url(" +
+      slideArray[i].background +
+      ");'>" +
+      "<h1>" +
+      slideArray[i].title +
+      "</h1>" +
+      "<h4>" +
+      slideArray[i].subtitle +
+      "</h4>" +
+      "</div>";
+  }
+  document.getElementById("mySlider").innerHTML = myHTML;
+  document.getElementById("slide" + currentSlideIndex).style.left = 0;
+}
+
+buildSlider();
+
+function prevSlide() {
+  var nextSlideIndex;
+  if (currentSlideIndex === 0) {
+    nextSlideIndex = slideArray.length - 1;
+  } else {
+    nextSlideIndex = currentSlideIndex - 1;
+  }
+  document.getElementById("slide" + nextSlideIndex).style.left = "-100%";
+  document.getElementById("slide" + currentSlideIndex).style.left = 0;
+    document
+    .getElementById("slide" + nextSlideIndex)
+    .setAttribute("class", "singleSlide slideInLeft");
+  document
+    .getElementById("slide" + currentSlideIndex)
+    .setAttribute("class", "singleSlide slideOutRight");
+  currentSlideIndex = nextSlideIndex;
+}
+function nextSlide() {
+  var nextSlideIndex;
+  if (currentSlideIndex === slideArray.length - 1) {
+    nextSlideIndex = 0;
+  } else {
+    nextSlideIndex = currentSlideIndex + 1;
+  }
+
+  document.getElementById("slide" + nextSlideIndex).style.left = "100%";
+  document.getElementById("slide" + currentSlideIndex).style.left = 0;
+
+  document
+    .getElementById("slide" + nextSlideIndex)
+    .setAttribute("class", "singleSlide slideInRight");
+  document
+    .getElementById("slide" + currentSlideIndex)
+    .setAttribute("class", "singleSlide slideOutLeft");
+
+  currentSlideIndex = nextSlideIndex;
+}
+setInterval(()=> nextSlide(), 5000)
+
+// canvas heart overlay
 const canvas = document.getElementById('falling-hearts');
   const ctx = canvas.getContext('2d');
   let numberHeart = 10;
@@ -74,5 +167,5 @@ const canvas = document.getElementById('falling-hearts');
     requestAnimationFrame(animate);
   }
 
-  createHearts(numberHeart); // Số lượng trái tim rơi
+  createHearts(numberHeart);
   animate();
